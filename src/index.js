74177519cddcb22262dce61846e0426680fbd03b
OLGA NARANOVICH <naranovicholga@gmail.com>
2018-09-19 10:42:26 +0300

    sorter

--- src/index.js
+++ src/index.js
@@ -30,33 +30,12 @@ class Sorter {
 		segment.sort(this.setComparator);
 		for (let j = 0; j < segment.length; j++) {
 			this.num[indxs[j]] = segment[j];
-		}
-		
-		// console.log(indxs);
-		// console.log(segment);
-
-		// return indxs;
-	 
+		}	 
 	}
 
 	setComparator(compareFunction) {
 		this.setComparator = compareFunction;
 	}
 }
-
-// const sorter2 = new Sorter();
-// console.log(sorter2.num);
-// sorter2.add(88);
-// sorter2.add(3);
-// sorter2.add(15);
-// sorter2.add(4);
-// sorter2.add(158);
-// sorter2.add(10);
-// console.log(sorter2.num);
-// sorter2.sort([4,3]);
-// sorter2.sort([4,5]);
-// console.log(sorter2.sort([4,3]));
-
-// console.log(sorter2.num);
 
 module.exports = Sorter;
